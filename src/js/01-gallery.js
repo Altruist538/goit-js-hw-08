995b7abdd8eddef281e06e20ce09d4b2a88654e5
Altruist538 <102470985+Altruist538@users.noreply.github.com>
2023-06-18 14:27:47 +0300

    22

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from '/node_modules/simplelightbox';
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+const galleryAdd = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a></li>`
+  )
+  .join('');
+galleryEl.innerHTML = galleryAdd;
+galleryEl.style.listStyle = 'none';
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,37 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const PLAYER_CURRENT_TIME = 'videoplayer-current-time';
+const playerEl = document.querySelector('#vimeo-player');
+const player = new Player(playerEl);
 
+// Функция для сохранения текущего времени воспроизведения в локальное хранилище
+const saveCurrentTime = throttle(() => {
+  player.getCurrentTime().then(currentTime => {
+    localStorage.setItem(PLAYER_CURRENT_TIME, JSON.stringify(currentTime));
+  });
+}, 1000);
+
+player.on('timeupdate', saveCurrentTime);
+// Функция для восстановления времени воспроизведения при загрузке страницы
+function restorePlaybackTime() {
+  const currentTime = localStorage.getItem(PLAYER_CURRENT_TIME);
+  if (currentTime) {
+    player
+      .setCurrentTime(parseFloat(currentTime))
+      .then(() => {
+        console.log('Playback time restored.');
+      })
+      .catch(function (error) {
+        switch (error.name) {
+          case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+          default:
+            // some other error occurred
+            break;
+        }
+      });
+  }
+}
+
+restorePlaybackTime();
