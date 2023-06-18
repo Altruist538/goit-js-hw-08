25b1187b4ee6d118ba057da72446f146d89c4cf7
Altruist538 <102470985+Altruist538@users.noreply.github.com>
2023-06-18 23:26:25 +0300

    третья задача - решение, вторая задача чистка кода

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,19 +4,15 @@ const PLAYER_CURRENT_TIME = 'videoplayer-current-time';
 const playerEl = document.querySelector('#vimeo-player');
 const player = new Player(playerEl);
 
-// Функция для сохранения текущего времени воспроизведения в локальное хранилище
 const saveCurrentTime = throttle(() => {
   player.getCurrentTime().then(currentTime => {
     localStorage.setItem(PLAYER_CURRENT_TIME, JSON.stringify(currentTime));
   });
 }, 1000);
-
 player.on('timeupdate', saveCurrentTime);
-// Функция для восстановления времени воспроизведения при загрузке страницы
+
 function restorePlaybackTime() {
   const currentTime = localStorage.getItem(PLAYER_CURRENT_TIME);
-
   player.setCurrentTime(currentTime || 0);
 }
-
 restorePlaybackTime();
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
+const inputObject = {};
+const FEEDBACK_FORM_STATE = 'feedback-form-state';
+const feedbackFormEl = document.querySelector('.feedback-form');
+const feedbackFormEmail = feedbackFormEl.querySelector('input[name="email"]');
+const feedbackFormMessage = feedbackFormEl.querySelector(
+  'textarea[name="message"]'
+);
 
+feedbackFormEl.addEventListener('input', throttle(setFeedbackForm, 500));
+feedbackFormEl.addEventListener('submit', getFeedbackForm);
+function setFeedbackForm(event) {
+  switch (event.target.name) {
+    case 'email':
+      inputObject['email'] = event.target.value;
+      break;
+    case 'message':
+      inputObject['message'] = event.target.value;
+      break;
+    default:
+      break;
+  }
+
+  localStorage.setItem(FEEDBACK_FORM_STATE, JSON.stringify(inputObject));
+}
+let output = JSON.parse(localStorage.getItem(FEEDBACK_FORM_STATE));
+
+function fillingForm(object) {
+  if (localStorage.getItem(FEEDBACK_FORM_STATE)) {
+    feedbackFormEmail.value = object.email;
+    feedbackFormMessage.value = object.message;
+  }
+}
+fillingForm(output);
+function getFeedbackForm(evt) {
+  evt.preventDefault();
+  console.log(output);
+  feedbackFormEmail.value = '';
+  feedbackFormMessage.value = '';
+  output = {};
+  localStorage.removeItem(FEEDBACK_FORM_STATE);
+}
