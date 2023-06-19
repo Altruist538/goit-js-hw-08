748a30282349a1bad8112e25b4fe1996c8b27335
Altruist538 <102470985+Altruist538@users.noreply.github.com>
2023-06-19 22:16:58 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,42 +1,25 @@
 import throttle from 'lodash.throttle';
-const inputObject = {};
 const FEEDBACK_FORM_STATE = 'feedback-form-state';
 const feedbackFormEl = document.querySelector('.feedback-form');
 const feedbackFormEmail = feedbackFormEl.querySelector('input[name="email"]');
 const feedbackFormMessage = feedbackFormEl.querySelector(
   'textarea[name="message"]'
 );
-
+let output = JSON.parse(localStorage.getItem(FEEDBACK_FORM_STATE)) || {
+  email: '',
+  message: '',
+};
+feedbackFormEmail.value = output.email;
+feedbackFormMessage.value = output.message;
 feedbackFormEl.addEventListener('input', throttle(setFeedbackForm, 500));
-feedbackFormEl.addEventListener('submit', getFeedbackForm);
 function setFeedbackForm(event) {
-  switch (event.target.name) {
-    case 'email':
-      inputObject['email'] = event.target.value;
-      break;
-    case 'message':
-      inputObject['message'] = event.target.value;
-      break;
-    default:
-      break;
-  }
-
-  localStorage.setItem(FEEDBACK_FORM_STATE, JSON.stringify(inputObject));
+  output[event.target.name] = event.target.value;
+  localStorage.setItem(FEEDBACK_FORM_STATE, JSON.stringify(output));
 }
-let output = JSON.parse(localStorage.getItem(FEEDBACK_FORM_STATE));
-
-function fillingForm(object) {
-  if (localStorage.getItem(FEEDBACK_FORM_STATE)) {
-    feedbackFormEmail.value = object.email;
-    feedbackFormMessage.value = object.message;
-  }
-}
-fillingForm(output);
+feedbackFormEl.addEventListener('submit', getFeedbackForm);
 function getFeedbackForm(evt) {
   evt.preventDefault();
   console.log(output);
-  feedbackFormEmail.value = '';
-  feedbackFormMessage.value = '';
-  output = {};
+  evt.target.reset();
   localStorage.removeItem(FEEDBACK_FORM_STATE);
 }
